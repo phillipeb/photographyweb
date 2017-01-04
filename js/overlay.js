bd4c21324a5804531ad38ea4fcbbe8e1ea51fe9b
nameofname <nameofemail@gmail.com>
2017-01-03 19:20:20 -0500

    more html indentation and clean up

--- js/overlay.js
+++ js/overlay.js
@@ -1,17 +1,24 @@
 'use strict';
-//javascript after that
 
-var owlArray = [{
-	large:”/images/ACCESSORIES-STILL/INSTYLE15/large/1.jpg”,
-	thumbs:”/images/ACCESSORIES-STILL/INSTYLE15/thumbs/1.jpg” 
-}, {
-	large:”/images/ACCESSORIES-STILL/TEENVOGUE/large/1.jpg”,
-	thumbs:”/images/ACCESSORIES-STILL/TEENVOGUE/thumbs/1.jpg” 
-}]
 
-// cahnge template to add the owl carousel class so it initializes properly.
-//underscore.template is a function that returns a function to var template/ so var template is a function in an of itself.
-var template = _.template("<div class="item"><img src="<%= obj.thumbs %>" /></div>", {attr : 'obj'}) // todo : make this template funciton work
+// var owlArray = [{
+// 	large:"/images/ACCESSORIES-STILL/INSTYLE15/large/1.jpg",
+// 	thumbs:"/images/ACCESSORIES-STILL/INSTYLE15/thumbs/1.jpg"
+// }, {
+// 	large:"/images/ACCESSORIES-STILL/TEENVOGUE/large/1.jpg",
+// 	thumbs:"/images/ACCESSORIES-STILL/TEENVOGUE/thumbs/1.jpg"
+// }];
+
+
+/**
+ * 1. on click of an image, grab json attribute off of a tag.
+ * @type {Function}
+ */
+
+
+
+// todo : make this template funciton work
+var template = _.template("<div class='item'><img src='<%= obj.thumbs %>' /></div>", {attr : 'obj'});
 
 $(".shmerds").click( function(e) {
 	e.preventDefault();
@@ -27,24 +34,25 @@ $(".shmerds").click( function(e) {
 	// show then .append() to overlay
 	// for unique array.
 	// call owl carousel to fit template
-	//for ajax yet to come: 
-	//$.ajax({
-	// 	url: "/json/",
-	// 	dataType : "json",
-	// 	beforeSend : function () {
-	// 		// todo : create a spinny wheel here to wait for your request
-	// 	},
-	// 	success : function(data, status, xhr) {
-	// 		debugger;
-	// 		console.log(data);
-	// 		data.forEach(function(obj) {
-	// 			var html = ''; // todo : here create html from your template // call the template function here
-	// 			$.append; // todo :: append the html that your template generated to the DOM here. 
-	// 		});
-	// 	},
-	// 	error : function() {
-	// 		console.log("fuck you");
-	// 	}
-	// });
-  
+	//for ajax yet to come:
+
+	$.ajax({
+		url: "/json/",
+		dataType : "json",
+		beforeSend : function () {
+			// todo : create a spinny wheel here to wait for your request
+		},
+		success : function(data, status, xhr) {
+			debugger;
+			console.log(data);
+			data.forEach(function(obj) {
+				var html = ''; // todo : here create html from your template // call the template function here
+				$.append; // todo :: append the html that your template generated to the DOM here.
+			});
+		},
+		error : function() {
+			console.log("fuck you");
+		}
+	});
+
 });
