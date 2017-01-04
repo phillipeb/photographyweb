f8f4252bb735c87487f89969b269df2ae45b24ec
nameofname <nameofemail@gmail.com>
2017-01-03 20:52:07 -0500

    replaced links to thumbs with links to large

--- js/overlay.js
+++ js/overlay.js
@@ -3,13 +3,14 @@
 
 var template = _.template("<div class='item'><img src='<%= obj.thumbs %>' /></div>", {attr : 'obj'});
 
-$("a.lightbox-thumb").click( function(e) {
-	e.preventDefault();
-
-	var carouselId = $(this).data('lightbox');
+$(".lightbox-thumbs a").click(function(e) {
 	debugger;
 
+	e.preventDefault();
+	var carouselId = $(this).data('lightbox');
 
+
+	debugger;
 
 	$('#fancyoverlay').html();
 
