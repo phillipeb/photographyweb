de16d66395e9b2e33e9e7ecdecd84e703225fc32
nameofname <nameofemail@gmail.com>
2017-01-03 20:27:15 -0500

    starting to move around the js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,8 @@
- //Image Slider 
-$(document).ready(function(){
-  $('.variable-width').slick({
-  dots: false,
-  infinite: true,
-  slidesToShow: 5,  
-  slidesToScroll: 3,
-  speed: 300,
-  centerMode: false,
-  variableWidth: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        infinite:true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth:true, 
-          dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth:true,
-      }
-    }
-  ]
-});
-   
-});
+//Image Slider
 
-//Slide Menu Nav 
-var 
-    wrapper    = $("#site-wrapper"),
-    menu       = $(".menu"),
-    menuLinks  = $(".menu ul li a"),
-    toggle     = $(".nav-toggle"),
-    toggleIcon = $(".nav-toggle span");
+//Slide Menu Nav
+var menu       = $(".menu"),
+    toggle     = $(".nav-toggle");
 
 function toggleThatNav() {
   if (menu.hasClass("show-nav")) {
@@ -59,7 +16,7 @@ function toggleThatNav() {
       menu.removeClass("show-nav");
       toggle.removeClass("show-nav");
     }
-    
+
   } else {
     if (!Modernizr.csstransforms) {
       menu.addClass("show-nav");
@@ -69,7 +26,7 @@ function toggleThatNav() {
     } else {
       menu.addClass("show-nav");
       toggle.addClass("show-nav");
-    } 
+    }
 
   }
 }
@@ -86,17 +43,57 @@ $(function() {
 
 //Services and Direction Image Section
 var slideIndex = 0;
-showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "list-item"; 
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
+  var slides = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex> slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "list-item";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+};
+
+
+$(document).ready(function(){
+  $('.variable-width').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    speed: 300,
+    centerMode: false,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          infinite:true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          variableWidth:true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth:true,
+        }
+      }
+    ]
+  });
+
+  showSlides();
+});
 
--- js/overlay.js
+++ js/overlay.js
@@ -1,39 +1,16 @@
 'use strict';
 
 
-// var template = _.template("<div class='item'><img src='<%= obj.thumbs %>' /></div>", {attr : 'obj'});
+var template = _.template("<div class='item'><img src='<%= obj.thumbs %>' /></div>", {attr : 'obj'});
 
-$("a ['data-lightbox']").click( function(e) {
+$("a.lightbox-thumb").click( function(e) {
 	e.preventDefault();
-	owlArray.forEach(function(content) {
-		var html = template(content)
-		$("#fancyoverlay").append(html);
-	});
 
-	// basically:
-	// overlay hidden
-	// show then .append() to overlay
-	// for unique array.
-	// call owl carousel to fit template
-	//for ajax yet to come:
+	var carouselId = $(this).data('lightbox');
+	debugger;
 
-	$.ajax({
-		url: "/json/",
-		dataType : "json",
-		beforeSend : function () {
-			// todo : create a spinny wheel here to wait for your request
-		},
-		success : function(data, status, xhr) {
-			debugger;
-			console.log(data);
-			data.forEach(function(obj) {
-				var html = ''; // todo : here create html from your template // call the template function here
-				$.append; // todo :: append the html that your template generated to the DOM here.
-			});
-		},
-		error : function() {
-			console.log("fuck you");
-		}
-	});
+
+
+	$('#fancyoverlay').html();
 
 });
