f3023871a4ae8997b0267da885e740d9fd68df17
nameofname <nameofemail@gmail.com>
2017-01-03 21:25:53 -0500

    implementing some of the overlay JS... with accompanied code clean up

--- js/overlay.js
+++ js/overlay.js
@@ -2,16 +2,60 @@
 
 
 var template = _.template("<div class='item'><img src='<%= obj.thumbs %>' /></div>", {attr : 'obj'});
+var opts = {
+	dots: false,
+	infinite: true,
+	slidesToShow: 5,
+	slidesToScroll: 3,
+	speed: 300,
+	centerMode: false,
+	variableWidth: true,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				infinite:true,
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				variableWidth:true,
+				dots: false
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				variableWidth:true,
+			}
+		}
+	]
+};
 
-$(".lightbox-thumbs a").click(function(e) {
-	debugger;
+$(document).ready(function() {
 
-	e.preventDefault();
-	var carouselId = $(this).data('lightbox');
+	$(".lightbox-thumbs a").click(function(e) {
 
+		e.preventDefault();
+		var carouselId = $(this).data('overlay-pointer');
+		var hiddenCarousel = $("[data-overlay='" + carouselId + "']").clone();
 
-	debugger;
+		$('#fancyoverlay')
+			.show()
+			.html(
+				hiddenCarousel.slick(opts)
+			);
 
-	$('#fancyoverlay').html();
+		$('#fancyoverlay a').click(function(e) {
+			e.preventDefault();
+		});
 
+	});
 });
